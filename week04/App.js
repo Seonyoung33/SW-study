498717569387bb9e3ca5e193a7c58a122c5efeb9
최소은 <cwy0505@naver.com>
2024-11-06 01:52:18 +0900

    add files

---
+++ week04/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import KakaoLogin from './KakaoLogin';
+
+const App = () => {
+  return (
+    <div>
+      <h1>카카오 로그인 예제</h1>
+      <KakaoLogin />
+    </div>
+  );
+};
+
+export default App;
